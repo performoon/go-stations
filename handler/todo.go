8e8ba0a23745ca61338c7ea8e12e6b728e6e054e
performoon <technikutryn@gmail.com>
2023-08-05 12:49:42 +0900

    s9

--- handler/todo.go
+++ handler/todo.go
@@ -56,6 +56,7 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(http.StatusBadRequest)
 			http.Error(w, "Bad Request: Invalid input", http.StatusBadRequest)
+			return
 			// w.Write()
 		} else {
 			todo, err := h.svc.CreateTODO(r.Context(), createTODORequest.Subject, createTODORequest.Description)
