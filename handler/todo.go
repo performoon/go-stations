5a7d13863fd88ec5eb57986206cfe729a08f133e
performoon <technikutryn@gmail.com>
2023-08-05 13:00:13 +0900

    s9

--- handler/todo.go
+++ handler/todo.go
@@ -54,12 +54,12 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		json.NewDecoder(r.Body).Decode(createTODORequest)
 		fmt.Println("Method=POST")
 		if createTODORequest.Subject == "" {
-			// w.Header().Set("Content-Type", "text/plain")
-			// w.WriteHeader(http.StatusBadRequest)
-			// http.Error(w, "Bad Request: Invalid input", http.StatusBadRequest)
-			w.Header().Set("Content-Type", "application/json") // レスポンスのContent-Typeを設定
+			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{"error": "Bad Request: Invalid input"})
+			http.Error(w, "Bad Request: Invalid input", http.StatusBadRequest)
+			// w.Header().Set("Content-Type", "application/json") // レスポンスのContent-Typeを設定
+			// w.WriteHeader(http.StatusBadRequest)
+			// json.NewEncoder(w).Encode(map[string]string{"error": "Bad Request: Invalid input"})
 			return
 			// w.Write()
 		} else if createTODORequest.Description == "" {
