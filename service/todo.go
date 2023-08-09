8d21e1e73d753a7e17c6940fa258b4e83bc6a421
performoon <technikutryn@gmail.com>
2023-08-09 15:38:20 +0900

    s12

--- service/todo.go
+++ service/todo.go
@@ -122,84 +122,84 @@ func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, descrip
 		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
 
-	stmt, err := s.db.PrepareContext(ctx, update)
-	if err != nil {
-		fmt.Println("PrepareContext err")
-		return nil, err
-	}
-	//defer stmt.Close()
-
-	result, err := stmt.ExecContext(ctx, subject, description, id)
-	//_, err = stmt.ExecContext(ctx, subject, description)
-	if err != nil {
-		fmt.Print("ExecContext err : ")
-		fmt.Println(err)
-		return nil, err
-	}
-
-	isRow, err := result.RowsAffected()
-	if isRow == 0 {
-		return nil, &model.ErrNotFound{}
-	}
-	insertID, err := result.LastInsertId()
-
-	fmt.Print("insert insertID : ")
-	fmt.Println(insertID)
-	fmt.Print("insertID type : ")
-	fmt.Println(reflect.TypeOf(insertID))
-
-	// _, err = stmt.Exec("UPDATE todos SET id = ?", insertID)
+	// stmt, err := s.db.PrepareContext(ctx, update)
 	// if err != nil {
-	// 	fmt.Print("ExecID err: ")
+	// 	fmt.Println("PrepareContext err")
+	// 	return nil, err
+	// }
+	// //defer stmt.Close()
+
+	// result, err := stmt.ExecContext(ctx, subject, description, id)
+	// //_, err = stmt.ExecContext(ctx, subject, description)
+	// if err != nil {
+	// 	fmt.Print("ExecContext err : ")
+	// 	fmt.Println(err)
+	// 	return nil, err
+	// }
+
+	// isRow, err := result.RowsAffected()
+	// if isRow == 0 {
+	// 	return nil, &model.ErrNotFound{}
+	// }
+	// insertID, err := result.LastInsertId()
+
+	// fmt.Print("insert insertID : ")
+	// fmt.Println(insertID)
+	// fmt.Print("insertID type : ")
+	// fmt.Println(reflect.TypeOf(insertID))
+
+	// // _, err = stmt.Exec("UPDATE todos SET id = ?", insertID)
+	// // if err != nil {
+	// // 	fmt.Print("ExecID err: ")
+	// // 	fmt.Println(err)
+	// // }
+
+	// stmt, err = s.db.PrepareContext(ctx, confirm)
+	// if err != nil {
+	// 	fmt.Println("PrepareContext err")
+	// 	return nil, err
+	// }
+	// //defer stmt.Close()
+	// fmt.Print("select PrepareContext : ")
+	// fmt.Println(stmt)
+
+	// _, err = stmt.ExecContext(ctx, insertID)
+	// //_, err = stmt.ExecContext(ctx, subject, description)
+	// if err != nil {
+	// 	fmt.Print("ExecContext err : ")
+	// 	fmt.Println(err)
+	// 	return nil, err
+	// }
+	// fmt.Print("select ExecContext : ")
+	// fmt.Println(result)
+
+	// todo := new(model.TODO)
+
+	// fmt.Print("insert insertID : ")
+	// fmt.Println(insertID)
+	// fmt.Print("insertID type : ")
+	// fmt.Println(reflect.TypeOf(insertID))
+
+	// todo.ID = int(insertID)
+
+	// //fmt.Println(todo)
+
+	// row := stmt.QueryRowContext(ctx, insertID)
+
+	// err = row.Scan(&todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+	// if err != nil {
+	// 	fmt.Print("row err : ")
 	// 	fmt.Println(err)
 	// }
 
-	stmt, err = s.db.PrepareContext(ctx, confirm)
-	if err != nil {
-		fmt.Println("PrepareContext err")
-		return nil, err
-	}
-	//defer stmt.Close()
-	fmt.Print("select PrepareContext : ")
-	fmt.Println(stmt)
+	// fmt.Print("todo : ")
+	// fmt.Println(todo)
+	// // row.Scan(todo)
 
-	_, err = stmt.ExecContext(ctx, insertID)
-	//_, err = stmt.ExecContext(ctx, subject, description)
-	if err != nil {
-		fmt.Print("ExecContext err : ")
-		fmt.Println(err)
-		return nil, err
-	}
-	fmt.Print("select ExecContext : ")
-	fmt.Println(result)
+	// // s.db.QueryRowContext(ctx, confirm)
 
-	todo := new(model.TODO)
-
-	fmt.Print("insert insertID : ")
-	fmt.Println(insertID)
-	fmt.Print("insertID type : ")
-	fmt.Println(reflect.TypeOf(insertID))
-
-	todo.ID = int(insertID)
-
-	//fmt.Println(todo)
-
-	row := stmt.QueryRowContext(ctx, insertID)
-
-	err = row.Scan(&todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
-	if err != nil {
-		fmt.Print("row err : ")
-		fmt.Println(err)
-	}
-
-	fmt.Print("todo : ")
-	fmt.Println(todo)
-	// row.Scan(todo)
-
-	// s.db.QueryRowContext(ctx, confirm)
-
-	//fmt.Println(todo)
-	fmt.Println("finish")
+	// //fmt.Println(todo)
+	// fmt.Println("finish")
 
 	return nil, nil
 }
