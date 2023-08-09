f123672df181df93adf191cd8b627267b351bf33
performoon <technikutryn@gmail.com>
2023-08-09 16:03:29 +0900

    s12

--- _test/sta12/sta12_test.go
+++ _test/sta12/sta12_test.go
@@ -110,7 +110,7 @@ func TestStation12(t *testing.T) {
 			}
 
 			want := &model.TODO{
-				ID:          tc.ID,
+				ID:          now(tc.ID),
 				Subject:     tc.Subject,
 				Description: tc.Description,
 				CreatedAt:   time.Now(),
